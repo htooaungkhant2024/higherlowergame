8c430a0857dffd52a6fb3d5f52ec5269eedba145
Htoo Aung Khant <161075690+htooaungkhant2024@users.noreply.github.com>
2024-12-18 16:01:25 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#display art
+
+from art import logo, vs
+
+#Generate a random account from the game data
+
+#format the account data into printable format
+
+#ask user for a guess
+
+#check if the user is correct 
+
+#get follower account of each account
+
+#use if statement to check if user is correct
+
+#give user feedback on their guess
+
+#score keeping 
+
+#Make the game repeatable 
+
